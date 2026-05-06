120d19b5cdb4e19ee38928d6d059a1336f0b137b
Keshab1997 <keshabsarkar2018@gmail.com>
2026-05-06 14:00:05 +0530

    chore(config): update Supabase credentials

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://eyiyyixotvneuvnbpfok.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5aXl5aXhvdHZuZXV2bmJwZm9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAwOTg4MzIsImV4cCI6MjA4NTY3NDgzMn0.NzKsPKRaHXoBg7GQk1lttY4G1WCKBs2gu6NEVr5J5fg";
+const SUPABASE_URL = "https://rudfyfvdhafgjomnwftz.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1ZGZ5ZnZkaGFmZ2pvbW53ZnR6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNDgwOTYsImV4cCI6MjA5MzYyNDA5Nn0.01PvIA1ocBF6mft0TYfqBZCdvaUu134IGr6DBj6uLc4";
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
